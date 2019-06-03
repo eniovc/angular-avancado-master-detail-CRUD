ee6abd22afc52cfa09caf5bea17b046d3150f141
Enio Vieira da Costa <enio.costa@aasp.com.br>
2019-06-03 18:02:28 -0300

    adicione a base de configuracao

---
+++ src/app/pages/categoria/categoria.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CategoriaRoutingModule } from './categoria-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CategoriaRoutingModule
+  ]
+})
+export class CategoriaModule { }
